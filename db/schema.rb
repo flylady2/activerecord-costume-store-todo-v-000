564173cd4183ffd47382b5ed45b778ace6453986
Leslie Stevens <flylady@mail.utexas.edu>
2018-08-28 20:50:25 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.integer "costume_inventory"
     t.integer "num_of_employees"
     t.boolean "still_in_business"
-    t.text "opening_time"
-    t.text "closing_time"
+    t.string "opening_time"
+    t.string "closing_time"
   end
 
   create_table "costumes", force: :cascade do |t|
@@ -27,8 +27,6 @@ ActiveRecord::Schema.define(version: 3) do
     t.float "price"
     t.integer "size"
     t.text "image_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "haunted_houses", force: :cascade do |t|

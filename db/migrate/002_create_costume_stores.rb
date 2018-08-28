350770814bac9e2ab814a1797fcc772ba216078b
Leslie Stevens <flylady@mail.utexas.edu>
2018-08-28 21:01:43 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,8 +7,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.text :opening_time
-      t.text :closing_time
+      t.text :opening_time, precision:9
+      t.text :closing_time, precision:9
     end
   end
 end# Create your costume_stores migration here

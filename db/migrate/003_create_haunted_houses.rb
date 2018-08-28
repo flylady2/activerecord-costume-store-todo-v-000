df829ad807dad33c4a81eb6227ff7e1a5b808d2f
Leslie Stevens <flylady@mail.utexas.edu>
2018-08-28 20:34:27 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,4 +4,11 @@ class HauntedHouses < ActiveRecord::Migration[5.1]
     create_table :haunted_houses do |t|
       t.string :name
       t.string :location
-      
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.string :opening_date
+      t.string :closing_date
+    end
+  end
+end
